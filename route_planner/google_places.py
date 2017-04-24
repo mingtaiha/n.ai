e11b157f692d2a87ea75b001954e93a8e7ec4578
mingtaiha <ming.tai.ha@gmail.com>
2017-04-24 09:30:26 -0400

    Minor changes

--- route_planner/google_places.py
+++ route_planner/google_places.py
@@ -7,57 +7,53 @@ YOUR_API_KEY = "AIzaSyCQENSbRFfd9_lGuqoXf2icRgtSvED-WHI"
 
 google_places = GooglePlaces(YOUR_API_KEY)
 
-# You may prefer to use the text_search API, instead.
-query_result = google_places.text_search(
-        location='Marlboro, New Jersey', query='Bank of America in Marlboro New Jersey',
-        radius=20000)
-# If types param contains only 1 item the request to Google Places API
-# will be send as type param to fullfil:
-# http://googlegeodevelopers.blogspot.com.au/2016/02/changes-and-quality-improvements-in_16.html
 
-print query_result
+def get_gplaces_results(loc):
+    # You may prefer to use the text_search API, instead.
+    query_result = google_places.text_search(query=loc, radius=20000)
+    # If types param contains only 1 item the request to Google Places API
+    # will be send as type param to fullfil:
+    # http://googlegeodevelopers.blogspot.com.au/2016/02/changes-and-quality-improvements-in_16.html
 
-if query_result.has_attributions:
-    print query_result.html_attributions
-    print
-
-for place in query_result.places:
-    # Returned places from a query are place summaries.
-    print place.name
-    print place.geo_location
-    print place.place_id
-    print
+    for place in query_result.places:
+        # Returned places from a query are place summaries.
+        print place.name
+        print place.geo_location
+        print place.place_id
 
 
-    # The following method has to make a further API call.
-    place.get_details()
-    # Referencing any of the attributes below, prior to making a call to
-    # get_details() will raise a googleplaces.GooglePlacesAttributeError.
-    pprint(place.details) # A dict matching the JSON response from Google.
-    pprint(place.local_phone_number)
-    pprint(place.international_phone_number)
-    pprint(place.website)
-    print place.url
+        # The following method has to make a further API call.
+        place.get_details()
+        # Referencing any of the attributes below, prior to making a call to
+        # get_details() will raise a googleplaces.GooglePlacesAttributeError.
+        #pprint(place.details) # A dict matching the JSON response from Google.
+#        pprint(place.local_phone_number)
+#        pprint(place.international_phone_number)
+#        pprint(place.website)
+        print place.formatted_address
+        #print place.formatted_phone_number
+        #print place.url
 
-    # Getting place photos
+    """
+        # Getting place photos
 
-    for photo in place.photos:
-        # 'maxheight' or 'maxwidth' is required
-        photo.get(maxheight=500, maxwidth=500)
-        # MIME-type, e.g. 'image/jpeg'
-        photo.mimetype
-        # Image URL
-        photo.url
-        # Original filename (optional)
-        photo.filename
-        # Raw image data
-        photo.data
+        for photo in place.photos:
+            # 'maxheight' or 'maxwidth' is required
+            photo.get(maxheight=500, maxwidth=500)
+            # MIME-type, e.g. 'image/jpeg'
+            photo.mimetype
+            # Image URL
+            photo.url
+            # Original filename (optional)
+            photo.filename
+            # Raw image data
+            photo.data
+    """
 
-
-# Are there any additional pages of results?
-if query_result.has_next_page_token:
-    query_result_next_page = google_places.nearby_search(
-            pagetoken=query_result.next_page_token)
+    # Are there any additional pages of results?
+    if query_result.has_next_page_token:
+        query_result_next_page = google_places.nearby_search(
+                pagetoken=query_result.next_page_token)
 
 """
 # Adding and deleting a place
--- route_planner/route_planner.py
+++ route_planner/route_planner.py
@@ -2,6 +2,7 @@ import os
 import time
 import utils
 from slackclient import SlackClient
+import google_places as gp
 
 #from environ variable
 BOT_ID = os.environ.get("ROUTE_PLANNER_ID")
@@ -11,10 +12,51 @@ AT_BOT = "<@" + BOT_ID + ">"
 
 PROXIMITY_CMD_UNIQ = ['nearest', 'closest']
 PROXIMITY_CMD = ['nearby', 'close by', 'near me', 'in the area', 'around me']
-EXAMPLE_COMMAND = "do"
+CLOSEST_ROUTE = "fastpath"
 
 #instantiate Slack client
 slack_client = SlackClient(os.environ.get('ROUTE_PLANNER_TOKEN'))
+
+#### Commands which our bot is designed to handle
+# What is the closest X nearby
+         # From current location, find X with min distance       
+# How do I get from X to Y
+        # Find X, Find Y, get from X to Y
+# Starting from X, I'd like to go to following places: X1, X2, ... , Xk
+        # Return or not return to X?
+# Starting from X, find me the fastest route to X1, X2, ...
+        # Return or not return to X?
+
+def closest_route(command):
+    start_loc = ""
+    end_loc = ""
+    while (start_loc == '') and (end_loc == ''):
+        locs = str(command).split('fastpath ')[1].split(',')
+        if 'from' == locs[0][:4]:
+            start_loc = locs[0][5:]
+            if 'to' in locs[1][:2]:
+                end_loc = locs[3:]
+            else:
+                response = "You are missing an ending location\n"
+                return response
+        elif 'to' in locs[0][:2]:
+            end_loc = locs[0][3:]
+            if 'from' in loc[1][:4]:
+                start_loc = locs[1][5:]
+            else:
+                response = "You are missing a starting location\n"
+                return response
+        else:
+            response = "try `fastpath from <start> to <end>`" 
+            return response
+            
+    gp.get_gplaces_results(start_loc)
+    print
+    print
+    gp.get_gplaces_results(end_loc)
+    response = "correct syntax\n"
+    return response
+
 
 
 def handle_command(command, channel):
@@ -24,15 +66,22 @@ def handle_command(command, channel):
         returns back what it needs for clarification.
     """
     response = "I'm not sure what you mean. Can you please repeat that?\n"
+    if command.startswith(CLOSEST_ROUTE):   
+        print command
+        closest_route(command)
+
+        
+
+    #cmd = utils.strip_punc(str(command))
+    #print cmd
+    #cmd_list = utils.filter_stopwords(cmd.split())
+
     
-    cmd = utils.strip_punc(str(command))
-    print cmd
-    cmd_list = utils.filter_stopwords(cmd.split())
 
     #if command.startswith(EXAMPLE_COMMAND):
-    response = unicode(" ".join(cmd_list))
-    print len(response)
-    slack_client.api_call("chat.postMessage", channel=channel,
+        response = unicode("seems to be working alright\n")
+        print len(response)
+        slack_client.api_call("chat.postMessage", channel=channel,
 							text=response, as_user=True)
 
 
